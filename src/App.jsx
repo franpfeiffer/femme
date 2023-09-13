69e5a3e00325452982da51d415048cff7328307c
Francisco Pfeiffer <pfeifferfran1@gmail.com>
2023-09-13 14:17:45 -0300

    Hice parte de la navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
-
+import './index.css';
+import './normalize.css';
+import React from 'react';
+import { Navbar } from './NavBar/Navbar'
 
 
 function App() {
   return (
     <>
-
+      <div>
+      <Navbar />
+    </div>
     </>
   )
 }
---
+++ src/NavBar/Navbar.jsx
@@ -0,0 +1,15 @@
+export const Navbar = () => {
+  return (
+    <Navbar>
+      <div className="logoFemme">LOGO</div>
+      <nav className="navBar">
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Productos</a></li>
+          <li><a href="#">Carrito</a></li>
+        </ul>
+      </nav>
+    </Navbar>
+  );
+};
+
