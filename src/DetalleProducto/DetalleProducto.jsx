03183c78e2a6831569f4167dd3cf0fc4f03288f6
MarcoSavarin0 <ateneo2003@gmail.com>
2023-10-09 11:18:47 -0300

    detalle de producto estiiizado

--- src/DetalleProducto/DetalleProducto.jsx
+++ src/DetalleProducto/DetalleProducto.jsx
@@ -1,7 +1,9 @@
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect, useContext } from 'react';
 import { useLocation } from 'react-router-dom';
 import { CarritoContext } from '../context/CarritoContext';
-
+import { Color } from 'three';
+import '../detalle-produ.css'
 export const DetalleProducto = () => {
     const [added, setAdded] = useState(false);
     const [producto, setProductos] = useState([]);
@@ -11,7 +13,11 @@ export const DetalleProducto = () => {
     const id = location.pathname.split('/')[1];
     const { agregarCompra, eliminarCompra, aumentarCantidad } = useContext(CarritoContext);
     const [amount, setAmount] = useState(1);
-    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+    const [colorSeleccionado, setColorSeleccionado] = useState(null);
+    const [talleSeleccionado, setTalleSeleccionado] = useState(null);
+    const [imagenSeleccionada, setImagenSeleccionada] = useState(imagene.length > 0 ? imagene[0].url : null);
+    const [mostrarAdvertencia, setMostrarAdvertencia] = useState(false);
+
 
     useEffect(() => {
         const fetchProductos = async () => {
@@ -40,58 +46,116 @@ export const DetalleProducto = () => {
         eliminarCompra(id)
         setAdded(false);
     }
-
-    const handlePrevImage = () => {
-        setCurrentImageIndex((prevIndex) => (prevIndex === 0 ? imagene.length - 1 : prevIndex - 1));
+    const handleThumbnailClick = (url) => {
+        setImagenSeleccionada(url);
+    };
+    const handleColorSeleccionado = (colorId) => {
+        setColorSeleccionado(colorId);
+    };
+    
+    // En el evento de selección de talle
+    const handleTalleSeleccionado = (talleId) => {
+        setTalleSeleccionado(talleId);
     };
 
-    const handleNextImage = () => {
-        setCurrentImageIndex((prevIndex) => (prevIndex === imagene.length - 1 ? 0 : prevIndex + 1));
-    };
+
 
     return (
-        <div className="product-detail">
-            <h2>{producto.nombre}</h2>
-            <div className="carousel-container">
-                <div className="carousel-images">
-                    {imagene.map((item, index) => (
-                        <img
-                            key={item.id}
-                            src={`http://localhost:3000/${item.url}`}
-                            alt="imagene"
-                            onError={() => {
-                                console.log('Error al cargar la imagen');
+        <div className="app">
+            <div className="details">
+                <div className="big-img">
+                    {imagenSeleccionada ? (
+                        <img src={`http://localhost:3000/${imagenSeleccionada}`} alt={producto.nombre} />
+                    ) : (
+                        imagene.length > 0 ? (
+                            <img src={`http://localhost:3000/${imagene[0].url}`} alt={producto.nombre} />
+                        ) : (
+                            <p>No image available</p>
+                        )
+                    )}
+                </div>
+
+                <div className="info">
+                    <div className='box'>
+                        <div className='row'>
+                            <h2>{producto.nombre}</h2>
+                            <span>${producto.precio}</span>
+                        </div>
+                    </div>
+                    <div className="colors">
+                        {prod_colores_talle.map(color => (
+                            <button key={color.id} style={{ background: color.colore.nombre }}
+                            onClick={() => {
+                                handleColorSeleccionado(color.colore.id);
+                                if (talleSeleccionado) {
+                                    setMostrarAdvertencia(false);
+                                } else {
+                                    setMostrarAdvertencia(true);
+                                }
+                            }}></button>
+                        ))}
+                    </div>
+                    <div className='talles'>
+                        {prod_colores_talle.map(talle => (
+                            <button key={talle.id}
+                            onClick={() => {
+                                handleTalleSeleccionado(talle.talle.id);
+                                if (colorSeleccionado) {
+                                    setMostrarAdvertencia(false);
+                                } else {
+                                    setMostrarAdvertencia(true);
+                                }
                             }}
-                            className={index === currentImageIndex ? 'carousel-image active' : 'carousel-image'}
-                        />
-                    ))}
+                            >{talle.talle.nombre}</button>
+                        ))}
+                    </div>
+
+
+
+                    <div className='thumb'>
+                        {imagene.map(img => (
+                            <img src={`http://localhost:3000/${img.url}`} alt="" key={img.id}
+                                className={img.url === imagenSeleccionada ? 'active' : ''}
+                                onClick={() => handleThumbnailClick(img.url)}
+
+                            />
+                        ))}
+                    </div>
+                    <p>{producto.descripcion}</p>
+                    {mostrarAdvertencia && <p>Por favor, seleccione un color y un talle antes de agregar al carrito.</p>}
+                    {prod_colores_talle.length > 0 ? (
+                        added ? (
+                            <button
+                                type="button"
+                                className="boton-quitar cart"
+                                onClick={() => {
+                                    if (colorSeleccionado && talleSeleccionado) {
+                                        setMostrarAdvertencia(false);
+                                        handleAgregar(producto);
+                                    } else {
+                                        setMostrarAdvertencia(true);
+                                    }
+                                }}
+                                disabled={mostrarAdvertencia}
+                            >
+                                Quitar del Carrito
+                            </button>
+                        ) : (
+                            <button
+                                type="button"
+                                className="boton-agregar cart"
+                                onClick={() => handleAgregar(producto)}
+                                disabled={colorSeleccionado === null || talleSeleccionado === null}
+                            >
+                                Agregar Carrito
+                            </button>
+                        )
+                    ) : (
+                        <button type="button" className="boton-agregar cart" disabled>
+                            Sin Stock
+                        </button>
+                    )}
                 </div>
-                <div className="carousel-buttons">
-                    <button onClick={handlePrevImage}>Anterior</button>
-                    <button onClick={handleNextImage}>Siguiente</button>
-                </div>
-                <p>Description: {producto.descripcion}</p>
-                <div>
-                    <button onClick={() => setAmount(amount > 1 ? amount - 1 : 1)}>-</button>
-                    <span>{amount}</span>
-                    <button onClick={() => setAmount(amount + 1)}>+</button>
-                </div>
-                {added
-                    ? <button
-                        type="button"
-                        className="boton-quitar"
-                        onClick={() => handleQuitar(producto.id)}
-                    >
-                        Quitar del Carrito
-                    </button>
-                    : <button
-                        type="button"
-                        className="boton-agregar"
-                        onClick={() => handleAgregar(producto)}
-                    >
-                        Agregar Carrito
-                    </button>
-                }
             </div>
         </div>
     );
